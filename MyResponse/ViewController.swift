b09378b3faa5b940e6aa4f9c7ac157e70740388b
4ecHok <89267225785@mail.ru>
2020-05-26 17:10:50 +0300

    first commit

---
+++ MyResponse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyResponse
+//
+//  Created by Сергей Цыганков on 26.05.2020.
+//  Copyright © 2020 Сергей Цыганков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
