fce27e4041979b777a3616c0e82b2e0309abdd11
4ecHok <89267225785@mail.ru>
2020-05-27 15:33:33 +0300

    end coding part

--- MyResponse/ViewController.swift
+++ MyResponse/ViewController.swift
@@ -24,10 +24,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-    
-    
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.isEmpty ? 0 : objects.count
     }
@@ -46,6 +42,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         cell.imageOfObject.layer.cornerRadius = cell.imageOfObject.frame.size.height / 2
         cell.imageOfObject.clipsToBounds = true
+        
+        if object.haveColor {
+           let id = setColor(mark: object.mark)
+            let backColor = UIColor(red: CGFloat(colors[id].red), green: CGFloat(colors[id].green), blue: CGFloat(colors[id].blue), alpha: CGFloat(colors[id].alpha))
+            cell.backgroundColor = backColor
+        } else {
+            cell.backgroundColor = .none
+        }
         
         return cell
     }
@@ -75,6 +79,65 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.reloadData()
     }
     
+    
+    //MARK: - new elements
+    
+    private func setColor(mark: Int ) -> Int {
+        var id = 0
+        
+        switch mark {
+        case 0...2:
+            id = 0
+        case 3...4:
+            id = 1
+        case 5...6:
+            id = 2
+        case 7...8:
+            id = 3
+        case 9...10:
+            id = 4
+        default:
+            id = 0
+        }
+        return id
+    }
+    
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let color = makeColor(at: indexPath)
+        
+        return UISwipeActionsConfiguration(actions: [color])
+    }
+    
+    //при перемещении ячейки неокрашенной на место окрашенной, она окрашивается
+    func makeColor(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .normal, title: "color") { (action, view, complite) in
+            
+            try! realm.write{
+            self.objects[indexPath.row].haveColor = !self.objects[indexPath.row].haveColor
+            }
+            self.makeBackground(at: indexPath)
+        }
+        action.backgroundColor = .blue
+        self.tableView.reloadData()
+        action.image = UIImage(systemName: "paintbrush")
+        return action
+    }
+    
+    private func makeBackground(at index: IndexPath) {
+        if self.objects[index.row].haveColor {
+            let id = self.setColor(mark: self.objects[index.row].mark)
+            let backColor = UIColor(red: CGFloat(self.colors[id].red),
+                                    green: CGFloat(self.colors[id].green),
+                                    blue: CGFloat(self.colors[id].blue),
+                                    alpha: CGFloat(self.colors[id].alpha))
+            self.tableView.cellForRow(at: index)!.backgroundColor = backColor
+        } else {
+            self.tableView.cellForRow(at: index)?.backgroundColor = .none
+        }
+        
+       
+    }
     
 }
 
